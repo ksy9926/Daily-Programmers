323a5ed9d9682bea7d92c66f9e78bd17554ce835
수영 김 <ksy9926@naver.com>
2021-04-30 17:38:21 +0900

    내적

---
+++ level 1/내적/solution.py
@@ -0,0 +1,10 @@
+def solution(a, b):
+    answer = 0
+    for i in range(len(a)):
+        answer += a[i] * b[i]
+    return answer
+
+# other solution
+
+def solution(a, b):
+    return sum([x*y for x, y in zip(a,b)])
