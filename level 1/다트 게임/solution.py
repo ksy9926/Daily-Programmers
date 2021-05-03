56fdd597f00eae6b16ca99340b36a1f8b906d87b
수영 김 <ksy9926@naver.com>
2021-05-03 15:17:19 +0900

    다트게임

---
+++ level 1/다트 게임/solution.py
@@ -0,0 +1,46 @@
+def solution(dartResult):
+    answer = 0
+    prev = []
+    cal = []
+    check_continue = False
+    for a in dartResult:
+        if a in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']:
+            if check_continue:
+                cal.pop()
+                cal.append(10)
+            else:
+                cal.append(int(a))
+                check_continue = True
+        elif a == 'S':
+            check_continue = False
+            continue
+        elif a == 'D':
+            cal[-1] = cal[-1] ** 2
+            check_continue = False
+        elif a == 'T':
+            cal[-1] = cal[-1] ** 3
+            check_continue = False
+        elif a == '*':
+            cal[-1] = cal[-1] * 2
+            if len(cal) >= 2:
+                cal[-2] = cal[-2] * 2
+        elif a == '#':
+            cal[-1] = -cal[-1]
+    answer = sum(cal)
+    return answer
+
+# 정규표현식 풀이
+import re
+
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+    answer = sum(dart)
+    return answer
