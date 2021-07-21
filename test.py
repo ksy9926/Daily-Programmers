bc01006ce9110c44f8d93a29b13f782b806f3d0b
수영 김 <ksy9926@naver.com>
2021-07-21 23:31:49 +0900

    JS: 서울에서 김서방 찾기

--- test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-s = "1D2S#10S"
-
-p = re.compile('(\d+)([SDT])([*#]?)')
-q = re.compile('\d+[SDT][*#]?')
-
-print(p.findall(s))  # [('1', 'D', ''), ('2', 'S', '#'), ('10', 'S', '')]
-print(q.findall(s))  # ['1D', '2S#', '10S']
