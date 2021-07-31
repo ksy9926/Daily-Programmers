11ec81a5779c781cc75cb8d4df7eeb03ccfd2470
수영 김 <ksy9926@naver.com>
2021-07-31 23:23:44 +0900

    JS: 예산

---
+++ level 1/기출문제/예산/solution.js
@@ -0,0 +1,15 @@
+function solution(d, budget) {
+  var answer = 0;
+
+  d.sort((a, b) => a - b);
+
+  for (let i = 0; i < d.length; i++) {
+    budget -= d[i];
+    if (budget < 0) {
+      break;
+    } else {
+    }
+    answer += 1;
+  }
+  return answer;
+}
