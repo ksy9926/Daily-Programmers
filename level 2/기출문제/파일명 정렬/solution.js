1c4faf86dbad322528b7c4c0d1f3fbb4343ffbb0
수영 김 <ksy9926@naver.com>
2021-08-12 12:00:56 +0900

    JS: 파일명 정렬

---
+++ level 2/기출문제/파일명 정렬/solution.js
@@ -0,0 +1,41 @@
+function solution(files) {
+  files.sort((a, b) => {
+    if (a.match(/\D+/)[0].toLowerCase() < b.match(/\D+/)[0].toLowerCase()) {
+      return -1;
+    } else if (
+      a.match(/\D+/)[0].toLowerCase() > b.match(/\D+/)[0].toLowerCase()
+    ) {
+      return 1;
+    } else {
+      if (Number(a.match(/\d{1,5}/)[0]) < Number(b.match(/\d{1,5}/)[0])) {
+        return -1;
+      } else if (
+        Number(a.match(/\d{1,5}/)[0]) < Number(b.match(/\d{1,5}/)[0])
+      ) {
+        return 1;
+      } else 1;
+    }
+  });
+
+  return files;
+}
+
+// 정규표현식 그룹 사용, 변수 설정
+function solution(files) {
+  let answerWrap = files.map((file, index) => ({ file, index }));
+  const compare = (a, b) => {
+    const reg = /(\D*)([0-9]*)/i;
+    const A = a.match(reg);
+    const B = b.match(reg);
+    const compareHead = A[1].toLowerCase().localeCompare(B[1].toLowerCase());
+    const compareNumber = (a, b) => {
+      return parseInt(a) > parseInt(b) ? 1 : parseInt(a) < parseInt(b) ? -1 : 0;
+    };
+    return compareHead === 0 ? compareNumber(A[2], B[2]) : compareHead;
+  };
+  answerWrap.sort((a, b) => {
+    const result = compare(a.file, b.file);
+    return result === 0 ? a.index - b.index : result;
+  });
+  return answerWrap.map((answer) => answer.file);
+}
